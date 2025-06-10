f50086e92e5a8b756f08ee15d32677693d53be1d
Luis Vasquez <luisangel.vp04@gmail.com>
2025-06-10 17:20:28 -0600

    Se crea archivo .env

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -16,7 +16,7 @@ import { LanguageService } from '../../services/language.service';
               <img 
                 [src]="isDarkMode ? 'https://assets.jasu.us/logos/jasuWH.png' : 'https://assets.jasu.us/logos/jasu.png'" 
                 alt="JASU Logo" 
-                class="h-12 w-auto"
+                class="h-10 w-auto"
               >
             </a>
           </div>
@@ -26,20 +26,14 @@ import { LanguageService } from '../../services/language.service';
             <a routerLink="/products" 
                routerLinkActive="active"
                class="nav-link">
-              <span class="flex items-center">
-                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/>
-                </svg>
+              <span>
                 {{ translations[currentLanguage].products }}
               </span>
             </a>
             <a routerLink="/calculator" 
                routerLinkActive="active"
                class="nav-link">
-              <span class="flex items-center">
-                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"/>
-                </svg>
+              <span>
                 {{ translations[currentLanguage].calculator }}
               </span>
             </a>
